d0f367b0c45d48cbd73c9698f8e0beb73adb7535
kobekovich <d.v.kove@gmail.com>
2019-11-20 13:41:49 +0200

    Random array + sortingexecution time

--- BubbleSort/bubbleSort.js
+++ BubbleSort/bubbleSort.js
@@ -1,6 +1,22 @@
-let inputArray = [0,-10000,9,7,-200,0,5,1,-999,0];
+let inputArray = [];
+let inputArrayLength = 10000;
+
+function fillInputArray() {
+    let multipliers = [-1000,-100,100,1000];
+    let multipliersLength = multipliers.length;
+    
+    for (let i = 1; i < inputArrayLength; i++) {
+        let randomMultiplier = Math.floor(Math.random() * multipliersLength);
+        let randomNumber = Math.round(Math.random() * multipliers[randomMultiplier]);
+        inputArray.push(randomNumber);
+    }
+
+    return console.log("Input array: " + inputArray);
+}
 
 function sort() {
+    let startTime = new Date();
+
     let j = 0;
     while (j < inputArray.length - 1) {
         for (let i = 0; i < inputArray.length - 1; i++) {
@@ -12,7 +28,11 @@ function sort() {
         }
         j++;
     }
-    return inputArray;
+
+    let finishTime = new Date() - startTime;
+
+    return console.log("Output array: " + inputArray + ". \nSort time : " + finishTime + " mss");
 }
-   
-console.log(sort());
+
+fillInputArray();
+sort();
